aa26dd7817cda85821ad5c18509719f40edbef33
BryanaKnight <brknig11@gmail.com>
2015-03-03 16:12:00 -0800

    Move get next guess into its own method

--- lib/game.rb
+++ lib/game.rb
@@ -19,17 +19,21 @@ class Game
     while guess.combo != "q" do
       if guess.too_short?
         puts Messages.too_short
-        @guess = Guess.new(gets.chomp.downcase)
+        get_next_guess
       elsif guess.too_long?
         puts Messages.too_long
-        @guess = Guess.new(gets.chomp.downcase)
+        get_next_guess
       elsif guess.combo != secret
         num_correct = GuessSecretEvaluator.number_letters_correct(guess.combo, secret)
         num_in_place = GuessSecretEvaluator.number_letters_correct_place(guess.combo, secret)
         puts Messages.wrong_guess(guess.combo.upcase, num_correct, num_in_place)
-        @guess = Guess.new(gets.chomp.downcase)
+        get_next_guess
       end
     end
+  end
+
+  def get_next_guess
+    @guess = Guess.new(gets.chomp.downcase)
   end
 
 end
